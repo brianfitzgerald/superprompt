fix masking loss

bert/trainer.py
@@ -67,15 +67,17 @@ class BERTTrainer:
         weight_decay: float = 0.01,
         warmup_steps=10000,
         max_len: int = 256,
-        with_cuda: bool = True,
-        cuda_devices=None,
         log_freq: int = 10,
         use_wandb: bool = False,
     ):
         # Setup cuda device for BERT training, argument -c, --cuda should be true
-        cuda_condition = torch.cuda.is_available() and with_cuda
-        self.device = torch.device("cuda:0" if cuda_condition else "cpu")
-
+        device = torch.device("cpu")
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+        elif torch.backends.mps.is_available():
+            device = torch.device("mps")
+        self.device = device
+        
         # This BERT model will be saved every epoch
         # Initialize the BERT Language Model, with BERT model
         self.model = bert.to(self.device)
@@ -83,11 +85,6 @@ class BERTTrainer:
         self.tokenizer = tokenizer
         self.collator = collator
         self.use_wandb = use_wandb
-
-        # Distributed GPU training if CUDA can detect more than 1 GPU
-        if with_cuda and torch.cuda.device_count() > 1:
-            print("Using %d GPUS for BERT" % torch.cuda.device_count())
-            self.model = nn.DataParallel(self.model, device_ids=cuda_devices)
 
         # Setting the train and test data loader
         self.train_data = train_dataset
@@ -103,7 +100,7 @@ class BERTTrainer:
         )
 
         # Using Negative Log Likelihood Loss function for predicting the masked_token
-        self.criterion = nn.NLLLoss(ignore_index=-100)
+        self.criterion = nn.NLLLoss(ignore_index=tokenizer.mask_token_id)
 
         self.log_freq = log_freq
         self.table_rows = []

train.py
@@ -11,9 +11,6 @@ import sys
 
 gc.collect()
 
-ava = torch.cuda.is_available()
-torch.cuda.empty_cache()
-
 args = Namespace(
     hidden=256,
     batch_size=32,
@@ -27,10 +24,8 @@ args = Namespace(
     save_freq=10,
     valid_freq=1,
     adam_beta2=0.999,
-    cuda_devices=[0],
     num_workers=4,
     lr=1e-3,
-    with_cuda=True,
     max_len=256,
     use_wandb=False
 )
@@ -41,9 +36,9 @@ if __name__ != "__main__":
 
 dataset = load_dataset("Gustavosta/Stable-Diffusion-Prompts", streaming=True)
 tokenizer: BertTokenizer = BertTokenizer.from_pretrained(
-    "bert-base-uncased", use_fast=True, mask_token="[MASK]"
+    "bert-base-uncased", use_fast=True
 )
-collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True, mlm_probability=0.15)
+collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True, mlm_probability=0.25)
 dataset = dataset.map(
     lambda x: tokenizer(
         x["Prompt"], truncation=True, padding="max_length", max_length=args.max_len, return_tensors="pt"
@@ -87,8 +82,6 @@ trainer = BERTTrainer(
     weight_decay=args.adam_weight_decay,
     max_len=args.max_len,
     log_freq=args.log_freq,
-    with_cuda=args.with_cuda,
-    cuda_devices=args.cuda_devices,
     use_wandb=args.use_wandb,
 )
 