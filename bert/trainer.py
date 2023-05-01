8bb4e555ad9be1404dc8be2a1d140ee7372a972b
Brian Fitzgerald <brianfitzgerald242@gmail.com>
2023-05-01 18:15:12 -0500

    update validation prompts

--- bert/trainer.py
+++ bert/trainer.py
@@ -14,9 +14,9 @@ from datasets import IterableDataset
 sample_prompts = [
     "human sculpture of lanky tall alien on a romantic date at italian restaurant with smiling woman, nice restaurant, photography, bokeh",
     "portrait of barbaric spanish conquistador, symmetrical, by yoichi hatakenaka, studio ghibli and dan mumford",
-    "a small liquid sculpture, corvette, viscous, reflective, digital art",
     "a beautiful painting of chernobyl by nekro, pascal blanche, john harris, greg rutkowski, sin jong hun, moebius, simon stalenhag. in style of cg art. ray tracing. cel shading. hyper detailed. realistic. ue 5. maya. octane render.",
-    "cyber moai on easter island, digital painting, highly detailed, concept art, trending on artstation, epic composition, sharp focus, flawless render, masterpiece, volumetric lighting",
+    "human sculpture of lanky tall alien on a romantic date at italian restaurant with smiling woman, nice restaurant, photography, bokeh",
+    "portrait of a rusian evil cyberpunk robot made of rusty clocks,dark styled, dramatic,extremely detailed, trending on artstation, cinematic view, beautiful composition, ambient light, fog, phenomenal photography",
 ]
 
 
