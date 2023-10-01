da35a051f7da4d618af36cb71e54a2fb55910ec5
Your Name <you@example.com>
2023-10-01 03:47:59 +0000

    add model wip

---
+++ models/emb_aug_model.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import os
+from typing import List
+import torch.nn.functional as F
+
+class EmbeddingAugModel(nn.Module):
+    def __init__(
+        self,
+        context_dim: int = 768,
+        layers: List[int] = [768, 1024, 1024, 768],
+        device="cpu",
+    ):
+        super().__init__()
+
+        self.dropout = nn.Dropout(0.1)
+        fc_layers = []
+        fc_layers.append(torch.nn.Linear(512, layers[0]))
+        for _, (in_size, out_size) in enumerate(zip(layers[:-1], layers[1:])):
+            fc_layers.append(torch.nn.Linear(in_size, out_size))
+            fc_layers.append(torch.nn.ReLU())
+        fc_layers.append(torch.nn.Linear(layers[-1], context_dim))
+        self.embed_fc = torch.nn.Sequential(*fc_layers).to(device)
+        self.bn = nn.BatchNorm1d(context_dim).to(device)
+
+    def forward(self, x):
+        if self.training:
+            x = self.dropout(x)
+        x = self.embed_fc(x)
+        return x
+
+    def grad_norm_sum(self):
+        grads = [
+            param.grad.detach().flatten()
+            for param in self.parameters()
+            if param.grad is not None and param.requires_grad
+        ]
+        total_norm = torch.cat(grads).norm()
+        return total_norm.item()
+
+    def weights_biases_sum(self):
+        total_weight_sum = 0.0
+        for param in self.parameters():
+            total_weight_sum += param.data.sum().item()
+        return total_weight_sum
--- train_emb.py
+++ train_emb.py
@@ -2,70 +2,140 @@ from argparse import Namespace
 from typing import Dict
 from datasets import load_dataset
 import spacy
+import wandb
+from models.emb_aug_model import EmbeddingAugModel
+import torch.nn as nn
+import fire
+import torch
+import torch.nn as nn
+import wandb
+from torch.optim import AdamW
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+
 
 from transformers import AutoTokenizer, CLIPTextModel
-from utils import should_use_wandb, get_available_device
-
-device = get_available_device()
-
-model = CLIPTextModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
-tokenizer = AutoTokenizer.from_pretrained("openai/clip-vit-base-patch32")
-max_clip_length = model.config.max_position_embeddings
+from utils import get_available_device
 
 if not spacy.util.is_package("en_core_web_sm"):
     spacy.cli.download("en_core_web_sm")
-nlp = spacy.load("en_core_web_sm")
 
+def main(use_wandb: bool = False):
+    device = get_available_device()
 
-class Args(Namespace):
-    use_wandb = should_use_wandb()
+    clip_model = CLIPTextModel.from_pretrained("openai/clip-vit-base-patch32").to(
+        device
+    )
+    tokenizer = AutoTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+    max_clip_length = clip_model.config.max_position_embeddings
 
+    nlp = spacy.load("en_core_web_sm")
 
-def mask_non_nouns(prompt):
-    doc = nlp(prompt)
-    masked_tokens = []
-    pos = set({"NOUN", "PROPN"})
-    for token in doc:
-        if token.pos_ in pos or (token.dep_ == "nsubj" and token.pos_ == "VERB"):
-            masked_tokens.append(token.text)
-    return " ".join(masked_tokens)
+    def mask_non_nouns(prompt):
+        doc = nlp(prompt)
+        masked_tokens = []
+        pos = set({"NOUN", "PROPN"})
+        for token in doc:
+            if token.pos_ in pos or (token.dep_ == "nsubj" and token.pos_ == "VERB"):
+                masked_tokens.append(token.text)
+        return " ".join(masked_tokens)
 
+    def preprocess_dataset(batch: Dict):
+        unmasked_prompts = batch["prompt"]
+        unmasked_inputs = tokenizer(
+            text=unmasked_prompts,
+            return_tensors="pt",
+            max_length=max_clip_length,
+            padding="max_length",
+            truncation=True,
+        ).to(device)
+        unmasked_embeddings = clip_model(**unmasked_inputs).last_hidden_state
 
-def process_batch(batch):
-    batch["masked_prompts"] = [mask_non_nouns(prompt) for prompt in batch["prompt"]]
-    return batch
+        masked_prompts = [mask_non_nouns(prompt) for prompt in unmasked_prompts]
+        masked_inputs = tokenizer(
+            text=masked_prompts,
+            return_tensors="pt",
+            max_length=max_clip_length,
+            padding="max_length",
+            truncation=True,
+        ).to(device)
+        masked_embeddings = clip_model(**masked_inputs).last_hidden_state
+        batch_dict = {
+            "unmasked_embeddings": unmasked_embeddings,
+            "masked_embeddings": masked_embeddings,
+            "masked_prompts": masked_prompts,
+            "unmasked_prompts": unmasked_prompts,
+        }
+        return batch_dict
 
+    # TODO filter for only high image text alignment scores
+    remove_cols = [
+        "image",
+        "prompt_id",
+        "prompt",
+        "classification",
+        "image_amount_in_total",
+        "rank",
+        "overall_rating",
+        "image_text_alignment_rating",
+        "fidelity_rating",
+    ]
+    dataset = load_dataset("THUDM/ImageRewardDB", "1k")
+    dataset = dataset.map(
+        preprocess_dataset,
+        batched=True,
+        num_proc=1,
+        batch_size=48,
+        remove_columns=remove_cols,
+    )
 
-def preprocess_dataset(batch: Dict):
-    unmasked_prompts = batch["prompt"]
-    unmasked_inputs = tokenizer(
-        text=unmasked_prompts,
-        return_tensors="pt",
-        max_length=max_clip_length,
-        padding="max_length",
-        truncation=True,
-    ).to(device)
-    unmasked_embeddings = model(**unmasked_inputs).last_hidden_state
+    model = EmbeddingAugModel(device=device)
 
-    masked_prompts = [mask_non_nouns(prompt) for prompt in unmasked_prompts]
-    masked_inputs = tokenizer(
-        text=masked_prompts,
-        return_tensors="pt",
-        max_length=max_clip_length,
-        padding="max_length",
-        truncation=True,
-    ).to(device)
-    masked_embeddings = model(**masked_inputs).last_hidden_state
-    batch_dict = {
-        "unmasked_embeddings": unmasked_embeddings,
-        "masked_embeddings": masked_embeddings,
-        "masked_prompts": masked_prompts,
-        "unmasked_prompts": unmasked_prompts,
-    }
-    return batch_dict
+    # display(model)
+    model.train()
+    num_epochs = 100
+    optimizer = AdamW(model.parameters(), lr=1e-5)
+    criterion = nn.MSELoss()
 
+    epoch = 0
 
-# TODO filter for only high image text alignment scores
-remove_cols = ['image', 'prompt_id', 'prompt', 'classification', 'image_amount_in_total', 'rank', 'overall_rating', 'image_text_alignment_rating', 'fidelity_rating']
-dataset = load_dataset("THUDM/ImageRewardDB", "1k")
-dataset = dataset.map(preprocess_dataset, batched=True, num_proc=1, batch_size=48, remove_columns=remove_cols)
+    if use_wandb:
+        wandb.init(project="superprompt-aug")
+        wandb.watch(model)
+
+    train_dataset, val_dataset = dataset["train"], dataset["validation"]
+    train_loader = DataLoader(train_dataset, batch_size=48)
+    val_loader = DataLoader(val_dataset, batch_size=len(val_dataset))
+
+    # Training loop
+    for epoch in range(num_epochs):
+        train_iter = tqdm(train_loader, total=len(train_loader))
+        for batch in train_iter:
+            mask_emb, unmask_emb = (
+                batch["masked_embeddings"],
+                batch["unmasked_embeddings"],
+            )
+
+            optimizer.zero_grad()
+
+            outputs = model(mask_emb)
+
+            loss = criterion(outputs, unmask_emb)
+            loss_formatted = "{:.4f}".format(loss.item())
+            log_dict = {
+                "loss": loss_formatted,
+                "epoch": epoch,
+                "lr": optimizer.param_groups[0]["lr"],
+            }
+            train_iter.set_postfix(log=log_dict)
+            if use_wandb:
+                wandb.log(log_dict)
+
+            # Backward pass and optimization
+            loss.backward()
+            optimizer.step()
+
+    wandb.finish()
+
+if __name__ == "__main__":
+    fire.Fire(main)
