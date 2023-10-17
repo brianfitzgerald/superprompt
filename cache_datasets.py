24d4151c48f1c3f7a25eaaeb4087d0e07144fc9f
Brian Fitzgerald <brianfitzgerald242@gmail.com>
2023-10-17 10:21:12 -0500

    upd

--- cache_datasets.py
+++ cache_datasets.py
@@ -7,6 +7,21 @@ import torch
 dataset = load_dataset("roborovski/diffusiondb-seq2seq")
 dataset = load_dataset("THUDM/ImageRewardDB", "4k", verification_mode="no_checks")
 dataset = load_dataset("bentrevett/multi30k")
+dataset = load_dataset(
+    "huggan/CelebA-HQ",
+    data_files={"train": "data/train-00000-of-00068.parquet"},
+    verification_mode="no_checks",
+)
+dataset = load_dataset(
+    "roborovski/celeba-faces-captioned",
+    data_files={
+        "train": [
+            "data/train-00000-of-00036-416615b669d11cd3.parquet",
+            "data/train-00001-of-00036-411c3786c0f93eac.parquet",
+        ]
+    },
+    verification_mode="no_checks",
+)
 pipe = StableDiffusionPipeline.from_pretrained(
     "runwayml/stable-diffusion-v1-5",
     torch_dtype=torch.float16,
