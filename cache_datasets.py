3b27b598bd53f4262167395f1d1682a76a808d1a
Your Name <you@example.com>
2023-10-17 13:51:34 +0000

    w

--- cache_datasets.py
+++ cache_datasets.py
@@ -1,5 +1,13 @@
 from datasets import load_dataset
+from diffusers import (
+    StableDiffusionPipeline,
+)
 
 dataset = load_dataset("roborovski/diffusiondb-seq2seq")
 dataset = load_dataset("THUDM/ImageRewardDB", "4k", verification_mode="no_checks")
 dataset = load_dataset("bentrevett/multi30k")
+pipe = StableDiffusionPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5",
+    torch_dtype=torch.float16,
+    safety_checker=None,
+)
