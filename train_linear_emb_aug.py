924bbea9b7bb4a3bf422d7ba1c8c58ab4ba45d8a
Your Name <you@example.com>
2023-11-25 01:58:29 +0000

    wip

--- train_linear_emb_aug.py
+++ train_linear_emb_aug.py
@@ -17,6 +17,8 @@ from utils import get_model_gradient_norm
 from torch.amp.autocast_mode import autocast
 from transformers import CLIPTextModel, AutoTokenizer, CLIPTokenizer
 
+torch.manual_seed(42)
+
 size = 512
 to_tensor = transforms.ToTensor()
 image_transforms = transforms.Compose(
@@ -54,22 +56,20 @@ def train(
     test_steps: int = 1000,
     test_batches: int = 10,
     output_filename: str = "linear_emb_aug.pt",
-    steps: float = 1e4,
     save_steps: int = 5000,
     batch_size: int = 8,
     num_dataloader_workers: int = 0,
-    n_epochs: int = 5,
+    n_epochs: int = 10,
     lr: float = 1e-4,
     clip_grad_val: float = 0,
     use_bnb: bool = True,
     use_wandb: bool = False,
 ):
     device = torch.device("cuda")
-    steps = int(steps)
 
-    text_encoder: CLIPTextModel = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14") # type: ignore
+    text_encoder: CLIPTextModel = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14")  # type: ignore
     text_encoder.to(device)  # type: ignore
-    tokenizer: CLIPTokenizer = AutoTokenizer.from_pretrained("openai/clip-vit-large-patch14") # type: ignore
+    tokenizer: CLIPTokenizer = AutoTokenizer.from_pretrained("openai/clip-vit-large-patch14")  # type: ignore
 
     dataset = load_dataset(
         "roborovski/upsampled-prompts-parti", verification_mode="no_checks"
@@ -88,6 +88,7 @@ def train(
         batch_size=batch_size,
         num_workers=num_dataloader_workers,
     )
+    n_steps = len(train_dataloader)
 
     model = LinearEmbAug().to(device)
 
@@ -108,20 +109,22 @@ def train(
     linear_scheduler = torch.optim.lr_scheduler.LinearLR(
         optimizer, start_factor=0.001, total_iters=200
     )
-    cosine_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, steps)
+    cosine_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, n_steps)
     scheduler = torch.optim.lr_scheduler.SequentialLR(
         optimizer, schedulers=[linear_scheduler, cosine_scheduler], milestones=[20]
     )
 
     model.train()
     epoch = 0
-    step = 0
-    progress_bar = tqdm(range(steps))
-    progress_bar.set_description("Steps")
+    global_step = 0
 
-    while epoch < n_epochs:
+    for epoch in range(n_epochs):
+
+        progress_bar = tqdm(range(n_steps))
+        progress_bar.set_description("Steps")
+
         for batch in train_dataloader:
-            step += 1
+            global_step += 1
 
             # get loss
             with autocast(device_type="cuda", dtype=torch.float16):
@@ -138,7 +141,9 @@ def train(
                 round(get_model_gradient_norm(model), 3),
                 scheduler.get_last_lr()[0],
             )
-            progress_bar.set_postfix(loss=loss_rounded, lr=lr_text, norm=norm_text, epoch=epoch)
+            progress_bar.set_postfix(
+                loss=loss_rounded, lr=lr_text, norm=norm_text, epoch=epoch
+            )
 
             if use_wandb:
                 wandb.log({"lr": lr_text, "norm": norm_text, "loss": loss})
@@ -150,13 +155,11 @@ def train(
             progress_bar.update(1)
             scheduler.step()
 
-            if step >= steps:
-                break
-            if (step % save_steps) == 0:
+            if (global_step % save_steps) == 0:
                 base, ext = os.path.splitext(output_filename)
-                save_filename = f"{base}-{step}{ext}"
+                save_filename = f"{base}-{global_step}{ext}"
                 torch.save(model.state_dict(), save_filename)
-            if step % test_steps == 0:
+            if global_step % test_steps == 0:
                 test_batches = 0
                 model.eval()
                 for batch in test_dataloader:
@@ -168,7 +171,6 @@ def train(
                     if test_batches >= test_batches:
                         break
                 model.train()
-        epoch += 1
 
     wandb.finish()
     torch.save(model.state_dict(), output_filename)
